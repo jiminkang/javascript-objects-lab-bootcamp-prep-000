8f18e002e253a48a79cd95cd982353ce0dd02950
Jimin Kang <jimin.kang91@gmail.com>
2017-12-19 04:28:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {eggs: 3}
 
 function updateObjectWithKeyAndValue(object,key,value){
-  return Object.assign({}, {[key]: value});
+  return Object.assign(object, {[key]: value});
   
 }
 
